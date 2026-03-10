5e29232ff85e6c6be762e4d4b1709d597d57baa4
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-10 13:24:28 -0600

    added console log to connectdb

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@ const dbPath = process.env.MONGO_URI;
 const connectDB = async () => {
     try {
         await mongoose.connect(dbPath);
+        console.log("MongoDB Connected")
     } catch (error) {
         console.log(error);
         process.exit(1);
