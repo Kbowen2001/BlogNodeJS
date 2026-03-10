5241868caf86f8fd4f16dba45df03f188a95ade5
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-10 12:16:27 -0600

    ad

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
+const PORT = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log('Server is running on port: ${PORT}'));
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
