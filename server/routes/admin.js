452508769e68e789a6b0dc596267f84fa9542f11
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-16 13:12:31 -0600

    adding to admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -34,6 +34,25 @@ router.get("/admin", async (req, res) => {
 });
 
 /**
+ * Check Login Middleware
+ */
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+};
+
+/**
  * Post /admin
  * Admin Login Submit
  */
