5bd08abb91c273024732b2c3cd9a2288c2112c81
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-16 14:44:16 -0600

    ad

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -309,5 +309,18 @@ router.put("/edit-post/:id", authMiddleware, async (req, res) => {
     });
 
 
+/**
+ * DELETE /delete-post
+ * Admin - Delete Post
+ */
+router.delete("/delete-post/:id", authMiddleware, async (req, res) => {
+    try {
+        await Post.deleteOne({ _id: req.params.id });
+        res.redirect("/dashboard");
+     } catch (error){
+        console.log(error);
+     }
+    });
+
 
 module.exports = router;
